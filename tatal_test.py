47527db415e9579836f997bb17cfc66ba9924161
chowoohyun <skywoohy93@gmail.com>
2022-09-01 14:54:42 +0900

    -______-
    ddkdkdkdk

--- tatal_test.py
+++ tatal_test.py
@@ -4,7 +4,11 @@ import face_expression
 # chat_bot 테스트
 #input_text = input('표정분석 결과를 입력 해주세요 :')
 for i in range(6):
+    file = open('result.txt', 'a')
     tmp = face_expression.face_dict[f'{i}']
-    #print(tmp)
+    tmp_answer = chat.chat_ai(tmp)
+    file.write(tmp_answer+'\n')
+
     print('Q :'+ tmp)
-    print('A : '+ chat.chat_ai(tmp))
+    print('A : '+ tmp_answer)
+file.close()
